3e268e6815852f8588275614f90ebc3971e0aa58
Jeron <norejudeisa@gmail.com>
2025-08-21 14:48:17 +0000

    Add Vercel Analytics for improved usage tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -84,6 +85,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster position={"top-center"} />
+        <Analytics />
       </body>
     </html>
   );
